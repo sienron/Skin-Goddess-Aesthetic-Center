c9264f9242b9e6e2af5526a907760d017fec076d
Christian <cpragides710@gmail.com>
2026-09-25 01:32:40 +0800

    fixed the Usermanagement design and Roles

--- index.js
+++ index.js
@@ -122,6 +122,7 @@ app.use('/api/services', require('./routes/services'));
 app.use('/api/appointments', require('./routes/appointments'));
 app.use('/api/availability', require('./routes/availability'));
 app.use('/api/inventory', require('./routes/inventory'));
+app.use('/api/users', require('./routes/users'));
 
 app.listen(PORT, () => {
   console.log(`Server is running http://localhost:${PORT}`);
